32a62f0d077c683c3134002fc1ed82cd603103ed
v.volkov3 <v.volkov3@tinkoff.ru>
2019-12-24 01:09:28 +0300

    Arrays added

--- src/ru/vvv/course/Hello.java
+++ src/ru/vvv/course/Hello.java
@@ -1,5 +1,26 @@
 package ru.vvv.course;
 
 public class Hello {
-    
+    public static void main(String[] args) {
+        int[] arr = new int[20];
+        int i = 0;
+        while (arr.length>i) {
+            arr[i] = i * 2;
+            System.out.print(arr[i] + " ");
+            i++;
+        }
+        System.out.println();
+
+        for (int j = 0; j < arr.length/2; j++) {
+            arr[j*2+1] *= arr[j*2];
+        }
+
+        for (int k:arr
+        ) {
+            System.out.print(k + " ");
+        }
+        System.out.println();
+
+        System.out.println("Hello");
+    }
 }
