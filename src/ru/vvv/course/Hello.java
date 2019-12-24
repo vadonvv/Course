03134c6f162ffec61370a482be643c4adacc9f23
vadonvv <vadonvv@github.com>
2019-12-24 19:38:47 +0300

    Arrays added

--- src/ru/vvv/course/Hello.java
+++ src/ru/vvv/course/Hello.java
@@ -2,25 +2,51 @@ package ru.vvv.course;
 
 public class Hello {
     public static void main(String[] args) {
+        //First
+        int[] arr = genArr();
+        for (int j = 0; j < arr.length/2; j++) {
+            arr[j*2+1] *= arr[j*2];
+        }
+        printArr(arr);
+
+        //Next
+        arr = genArr();
+        int tmp =0;
+        int k = 0;
+        do{
+            if(k%2==1)
+                arr[k]*=tmp;
+            else
+                tmp=arr[k];
+            k++;
+        }while(k<arr.length);
+        printArr(arr);
+
+        //Next
+        arr = genArr();
+        int j = 0;
+        while(arr.length/2>j){
+            arr[j*2+1]*=arr[j*2];
+            j++;
+        }
+        printArr(arr);
+    }
+
+    private static int[] genArr(){
         int[] arr = new int[20];
         int i = 0;
         while (arr.length>i) {
             arr[i] = i * 2;
-            System.out.print(arr[i] + " ");
             i++;
         }
-        System.out.println();
+        return arr;
+    }
 
-        for (int j = 0; j < arr.length/2; j++) {
-            arr[j*2+1] *= arr[j*2];
-        }
-
+    private  static void printArr(int arr[]){
         for (int k:arr
         ) {
             System.out.print(k + " ");
         }
         System.out.println();
-
-        System.out.println("Hello");
     }
 }
